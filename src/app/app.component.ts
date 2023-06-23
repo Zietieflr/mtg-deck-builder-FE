f2d5e937175a024d07c76e68952c16f947b56a8a
Zietieflr <lrmcguire93@gmail.com>
2023-06-23 01:21:53 -0600

    Add initial component structure. Add dark theme styling.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from "@angular/core";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { RouterOutlet } from "@angular/router";
 
 @Component({
-    selector: "app-root",
-    templateUrl: "./app.component.html",
-    styleUrls: ["../../node_modules/reset-css/reset.css", "./app.component.scss"],
-    standalone: true,
-    imports: [RouterOutlet],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["../../node_modules/reset-css/reset.css", "./app.component.scss"],
+  standalone: true,
+  imports: [MatIconModule, MatTabsModule, MatToolbarModule, RouterOutlet],
 })
 export class AppComponent {
   public title = "deck-builder";
