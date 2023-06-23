27a58fa30ff0325202ee842f3fe3adb0c3a5b70d
Zietieflr <lrmcguire93@gmail.com>
2023-06-23 00:33:13 -0600

    Convert to standalone component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["../../node_modules/reset-css/reset.css", "./app.component.scss"],
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["../../node_modules/reset-css/reset.css", "./app.component.scss"],
+    standalone: true,
+    imports: [RouterOutlet],
 })
 export class AppComponent {
   public title = "deck-builder";
