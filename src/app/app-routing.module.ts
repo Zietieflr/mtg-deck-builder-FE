872d86b094efdf8979a50cf2a269718f1cf0c775
Zietieflr <lrmcguire93@gmail.com>
2023-07-10 22:43:16 -0600

    feat: Add touting foundation for main component and page not found.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { MainComponent } from "deck-builder/core/main/main.component";
+import { SearchContainerComponent } from "deck-builder/search-for-cards/search-container/search-container.component";
+import { PageNotFoundComponent } from "deck-builder/page-not-found/page-not-found/page-not-found.component";
 
-const routes: Routes = [{ path: "", component: MainComponent }];
+const routes: Routes = [
+  {
+    path: "",
+    component: MainComponent,
+    children: [
+      { path: "search", component: SearchContainerComponent },
+      { path: "", redirectTo: "/search", pathMatch: "full" },
+    ],
+  },
+  { path: "**", component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
