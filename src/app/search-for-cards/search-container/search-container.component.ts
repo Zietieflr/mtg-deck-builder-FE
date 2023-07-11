3652442ba58542f47de88eaf13fe9083bf84e7de
Zietieflr <lrmcguire93@gmail.com>
2023-07-10 22:33:03 -0600

    feat: Inittialize search feature component.

---
+++ src/app/search-for-cards/search-container/search-container.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-search-container',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './search-container.component.html',
+  styleUrls: ['./search-container.component.scss']
+})
+export class SearchContainerComponent {
+
+}
