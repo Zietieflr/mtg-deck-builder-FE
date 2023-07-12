e26ccc165f392ddd469ed8e9b7cb5d19ef7d9330
Zietieflr <lrmcguire93@gmail.com>
2023-07-11 23:25:37 -0600

    test: Fix broken test config and organize imports.

--- src/app/core/main/main.component.spec.ts
+++ src/app/core/main/main.component.spec.ts
@@ -1,10 +1,14 @@
-import { MainComponent } from "deck-builder/core/main/main.component";
+import { ActivatedRoute } from "@angular/router";
 import { Spectator, createComponentFactory } from "@ngneat/spectator";
+import { MainComponent } from "deck-builder/core/main/main.component";
 
 describe("MainComponent", () => {
   let spectator: Spectator<MainComponent>;
 
-  const createComponent = createComponentFactory(MainComponent);
+  const createComponent = createComponentFactory({
+    component: MainComponent,
+    mocks: [ActivatedRoute],
+  });
 
   beforeEach(() => {
     spectator = createComponent();
--- src/app/core/main/main.component.ts
+++ src/app/core/main/main.component.ts
@@ -1,5 +1,5 @@
-import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
 import { MatDividerModule } from "@angular/material/divider";
 import { MatIconModule } from "@angular/material/icon";
 import { MatTabsModule } from "@angular/material/tabs";
