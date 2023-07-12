d4ba17005bb251e6d1f890bad75ab95caf55a2ef
Zietieflr <lrmcguire93@gmail.com>
2023-07-12 01:03:19 -0600

    style: Run linting/formatting.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { MainComponent } from "deck-builder/core/main/main.component";
-import { SearchContainerComponent } from "deck-builder/search-for-cards/search-container/search-container.component";
 import { PageNotFoundComponent } from "deck-builder/page-not-found/page-not-found/page-not-found.component";
+import { SearchContainerComponent } from "deck-builder/search-for-cards/search-container/search-container.component";
 
 const routes: Routes = [
   {
