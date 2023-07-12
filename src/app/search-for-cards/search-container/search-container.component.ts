bd019c2c56cb8b2abc3c702162647e68849d1605
Zietieflr <lrmcguire93@gmail.com>
2023-07-11 23:29:55 -0600

    feat: Initial scaffolding and light styling for search for card(s) feature.

--- src/app/search-for-cards/search-container/search-container.component.ts
+++ src/app/search-for-cards/search-container/search-container.component.ts
@@ -1,10 +1,20 @@
-import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
 
 @Component({
   selector: "app-search-container",
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
   templateUrl: "./search-container.component.html",
   styleUrls: ["./search-container.component.scss"],
 })
